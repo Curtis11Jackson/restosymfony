bbba349d10688ee76bcf137ce50f1444e71a9a03
c.jackson <jacksoncurt11@gmail.com>
2020-04-02 09:52:18 +0200

    premmier commit

---
+++ src/Controller/RestoController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RestoController extends AbstractController
+{
+    /**
+     * @Route("/resto", name="resto")
+     */
+    public function index()
+    {
+        return $this->render('resto/index.html.twig', [
+            'controller_name' => 'RestoController',
+        ]);
+    }
+
+      /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('resto/home.html.twig');
+    }
+}
